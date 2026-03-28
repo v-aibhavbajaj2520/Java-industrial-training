0857f6729c60b125a368c4ac0e46fdf77dd1a81a
Vaibhav Bajaj <vaibhavbajaj20@gmail.com>
2026-03-28 11:52:17 +0530

    Create Rod-Cutting.java
    GFG Rod Cutting question

---
+++ GFG/Rod-Cutting.java
@@ -0,0 +1,14 @@
+// Rod Cutting
+class Solution {
+    public int cutRod(int[] price) {
+        // code here
+        int n = price.length;
+        int []dp = new int[n + 1];
+        for (int i = 1; i <= n; i++){
+                for (int j = 1; j <= i; j++){
+                    dp[i] = Math.max(dp[i], dp[i - j] + price[j - 1]);
+                }
+        }
+        return dp[n];
+    }
+}
