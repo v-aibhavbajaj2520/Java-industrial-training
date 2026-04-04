89e4de9e5596384923836e0a2ee090b37fced28f
Vaibhav-Bajaj <vaibhavbajaj20@gmail.com>
2026-04-04 12:26:49 +0530

    Create generate-parenthesis.java

---
+++ Leetcode/generate-parenthesis.java
@@ -0,0 +1,28 @@
+//22 -> Generate Parentheses
+// https://leetcode.com/problems/generate-parentheses/
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        ArrayList<String> res= new ArrayList<>();
+        solve(n, 0,0,new StringBuilder(),res);
+        return res;
+    }
+    void solve(int n, int l, int r, StringBuilder s, List<String> res){
+        if(s.length()==2*n){
+            res.add(s.toString());
+            return;
+        }
+        if(l<n){
+            s.append('(');
+            solve(n, l+1, r, s, res);
+            s.deleteCharAt(s.length()-1);
+        }
+        
+        if(r<l){
+            s.append(')');
+            solve(n, l, r+1, s, res);
+            s.deleteCharAt(s.length()-1);
+        }
+
+    }
+}
